72f33a9d63e5bb179f9f2151d78acac3604b8a00
Sergey Otrubyannikov <s.otrubyannikov@gmail.com>
2024-03-28 00:04:45 +0300

    modified index.html

--- src/main.tsx
+++ src/main.tsx
@@ -51,7 +51,7 @@ const router = createBrowserRouter(
       element: <PageTerms />,
     },
   ],
-  { basename: "/neoflex-test/" }
+  { basename: import.meta.env.BASE_URL }
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
